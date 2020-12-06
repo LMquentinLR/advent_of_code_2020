56930593fc07abb78f82350a5fada841388f3da2
Quentin Le Roux <quentin.leroux@edhec.com>
2020-12-06 22:46:27 +0100

    day 3 completed

--- day_3/day3.hs
+++ day_3/day3.hs
@@ -1,5 +1,43 @@
+import Data.List (genericIndex)
 
+type Index = Int
+type Slope = Int
+type MoveDown = Int
+
+cycleElement :: [Char] -> Int -> Int
+cycleElement element index = tree
+    where 
+        list = cycle element
+        item = list `genericIndex` index
+        tree = 
+            if item == '#'
+            then 1
+            else 0
+
+-- recursiveReadOld :: [[Char]] -> Int -> [Int]
+-- recursiveReadOld [] _ = []
+-- recursiveReadOld (x:xs) index = (cycleElement x index) : (recursiveRead xs $ index + 3)
+
+recursiveRead :: [[Char]] -> Index -> (Slope, MoveDown) -> [Int]
+recursiveRead [] _ _ = []
+recursiveRead (x:xs) index strategy = processedArea : nextArea
+    where 
+        slope = fst strategy
+        nextList = 
+            if length xs < 2
+            then []
+            else tail xs
+        foreground = 
+            if snd strategy == 1
+            then xs
+            else nextList
+        processedArea = cycleElement x index
+        nextArea = recursiveRead foreground (index + slope) strategy
 
 main :: IO ()
 main = do
-    print "done"
+    ls <- readFile "input.txt"
+    let unpackedLs = lines ls
+    let strategy = [(1,1), (3,1), (5,1), (7,1), (1,2)]::[(Slope, MoveDown)]
+    let processing = recursiveRead unpackedLs 0 
+    print $ (foldl (*) 1) $ map (foldl (+) 0) $ map processing strategy
