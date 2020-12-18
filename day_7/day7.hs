a0821f790b1d27dc24a1a997e2778b6aaa33cc36
LMquentinLR <quentin.leroux@edhec.com>
2020-12-18 02:31:26 +0100

    day 7 full

--- day_7/day7.hs
+++ day_7/day7.hs
@@ -1,6 +1,6 @@
 import Data.List.Split ( splitOn )
-import Data.Graph ( graphFromEdges, transposeG, reachable, Graph )
-import Data.Maybe ( fromMaybe, isJust )
+import Data.Graph ( graphFromEdges, transposeG, reachable )
+import Data.List ( group, sort )
 
 type Instruction = (String, String, [String]) 
 
@@ -14,17 +14,47 @@ parseLine ln = output
     bagColors = fmap (concat . reverse . take 2 . reverse . splitOn " ") splitLine
     output = (head bagColors, head bagColors, tail bagColors)
 
-parseInstructions :: [String] -> [Instruction]
-parseInstructions = map parseLine
+createInstructions :: String -> (String, String) -> [Instruction]
+createInstructions bag occurences
+    | occurence == "contain" = [(bag, bag, [subBag])]
+    | otherwise        = concatMap (replicate (read occurence :: Int)) [(bag, bag, [subBag])] 
+  where 
+    occurence = fst occurences
+    subBag = snd occurences
 
-canReachShinyGold :: [Instruction] -> Maybe Int
-canReachShinyGold instructionSet = output
+parseLineDuplicates :: String -> [Instruction]
+parseLineDuplicates ln = concatMap (createInstructions $ head bagColors) occurences 
+    where
+      splitLine = init $ splitOn " bag" ln
+      splitItems = (:) (head splitLine) <$> fmap lines (tail splitLine)
+      bagNumber = fmap (last . take 3 . reverse . splitOn " " . head .  tail) splitItems
+      bagColors = fmap (concat . reverse . take 2 . reverse . splitOn " ") splitLine
+      occurences = zip bagNumber $ tail bagColors
+
+countComponents :: Bool -> [Instruction] -> Maybe Int
+countComponents invert instructionSet = output
   where 
     (graph, nodeFromVertex, vertexFromKey) = graphFromEdges instructionSet
     shinyGoldVertex = vertexFromKey "shinygold"
-    output = fmap (\x -> x-1) $ length . reachable (transposeG graph) <$> shinyGoldVertex
+    finalGraph 
+      | invert    = transposeG graph
+      | otherwise = graph
+    output = fmap (\x -> x-1) $ length . reachable finalGraph <$> shinyGoldVertex
+
+recursiveCount :: [(Instruction, Int)] -> (Int, String) -> Int
+recursiveCount dataset (holdValue, name) = (+) (sum bagNumber) recursiveBit
+  where
+    filteredDataset = filter (\((_, _, [z]), _) -> z /= "noother") $ 
+      filter (\((x, _, _), _) -> x == name) dataset
+    bagNumber = map (\((_,_,_),t) -> holdValue * t) filteredDataset
+    nextStep = zip bagNumber $ map (\((_, _, [z]), _) -> z) filteredDataset
+    recursiveBit = sum $ map (recursiveCount dataset) nextStep
 
 main :: IO()
 main = do
     instructions <- readContent
-    print $ canReachShinyGold (parseInstructions instructions) 
+    let mapIns = map (\x -> (head x, length x)) . group . sort $ concatMap parseLineDuplicates instructions
+    print $ countComponents True (map parseLine instructions) 
+    print $ recursiveCount mapIns (1, "shinygold")
+
+
